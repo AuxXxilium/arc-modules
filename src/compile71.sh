4d8256690b7b68e410d6c96116fb14ed16c6600f
AuxXxilium <info@auxxxilium.tech>
2025-11-06 00:19:59 +0100

    tree: update more
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>

--- src/compile71.sh
+++ src/compile71.sh
@@ -4,36 +4,44 @@ set -e
 
 TOOLKIT_VER="7.1"
 
-#if [ -f ../../arpl/PLATFORMS ]; then
-#  cp ../../arpl/PLATFORMS PLATFORMS
-#else
-#  curl -sLO "https://github.com/fbelavenuto/arpl/raw/main/PLATFORMS"
-#fi
-
 echo -e "Compiling modules..."
 while read PLATFORM KVER; do
   [ -n "$1" -a "${PLATFORM}" != "$1" ] && continue
   DIR="${KVER:0:1}.x"
   [ ! -d "${PWD}/${DIR}" ] && continue
   mkdir -p "/tmp/${PLATFORM}-${KVER}"
-  #docker run --rm -t -v "${PWD}/${1}/${DIR}":/input -v "${PWD}/../${PLATFORM}-${KVER}":/output \
-  #  fbelavenuto/syno-toolkit:${PLATFORM}-${TOOLKIT_VER} compile-module
+
+  # Ensure the defines.x file is respected
+  DEFINES_FILE="defines.${PLATFORM}"
+  if [ -f "${PWD}/${DIR}/${DEFINES_FILE}" ]; then
+    cp "${PWD}/${DIR}/${DEFINES_FILE}" "${PWD}/${DIR}/.config"
+  else
+    echo "Warning: ${DEFINES_FILE} not found for ${PLATFORM}"
+  fi
+
+  # Prepare the Docker run parameters
   runparam=$(echo "-u `id -u` --rm -t -v "${PWD}/${DIR}":/input -v "/tmp/${PLATFORM}-${KVER}":/output \
     fbelavenuto/syno-compiler:${TOOLKIT_VER} compile-module ${PLATFORM}")
-  echo $runparam  
+  echo $runparam
+
+  # Run the Docker container
   docker run -u `id -u` --rm -t -v "${PWD}/${DIR}":/input -v "/tmp/${PLATFORM}-${KVER}":/output \
     fbelavenuto/syno-compiler:${TOOLKIT_VER} compile-module ${PLATFORM}
-    if [ "${PLATFORM}" = "epyc7002" ]; then
-      PLATFORM_DIR="${PLATFORM}-${TOOLKIT_VER}-${KVER}"
-    else
-      PLATFORM_DIR="${PLATFORM}-${KVER}"
-    fi
-    rm -rf ${PWD}/../${PLATFORM_DIR}
+
+  # Handle output directory naming
+  if [ "${PLATFORM}" = "epyc7002" ]; then
+    PLATFORM_DIR="${PLATFORM}-${TOOLKIT_VER}-${KVER}"
+  else
+    PLATFORM_DIR="${PLATFORM}-${KVER}"
+  fi
+  rm -rf ${PWD}/../${PLATFORM_DIR}
+
+  # Copy compiled modules and clean up
   for M in `ls /tmp/${PLATFORM}-${KVER}`; do
     [ -f ~/src/pats/modules/${PLATFORM}/$M ] && \
     cp ~/src/pats/modules/${PLATFORM}/$M "${PWD}/../${PLATFORM_DIR}/" || \
     { mkdir -p "${PWD}/../${PLATFORM_DIR}" && cp /tmp/${PLATFORM}-${KVER}/$M "${PWD}/../${PLATFORM_DIR}/"; }
-    # remove i915 related modules
+    # Remove unwanted modules
     [[ -f ${PWD}/../${PLATFORM}-${KVER}/cfbfillrect.ko ]] && rm ${PWD}/../${PLATFORM_DIR}/cfbfillrect.ko 
     [[ -f ${PWD}/../${PLATFORM}-${KVER}/cfbimgblt.ko ]] && rm ${PWD}/../${PLATFORM_DIR}/cfbimgblt.ko 
     [[ -f ${PWD}/../${PLATFORM}-${KVER}/cfbcopyarea.ko ]] && rm ${PWD}/../${PLATFORM_DIR}/cfbcopyarea.ko 
