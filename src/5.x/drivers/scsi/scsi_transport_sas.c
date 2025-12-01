d3e9521d43ff365d7488af1c5c80ab8cf9299f6b
AuxXxilium <info@auxxxilium.tech>
2025-12-01 20:14:43 +0100

    Revert "tree: scsi add
    Signed-off-by: AuxXxilium <info@auxxxilium.tech>"
    
    This reverts commit e843185828eac92fdf14988e72aa6e13cf987de1.

--- src/5.x/drivers/scsi/scsi_transport_sas.c
+++ src/5.x/drivers/scsi/scsi_transport_sas.c
@@ -22,7 +22,7 @@
  * what PHYs form a wide port based on the port_identifier attribute,
  * which is the same for all PHYs in a port.
  */
-
+#define CONFIG_SCSI_SAS_ATTRS_MODULE 1
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/jiffies.h>
@@ -1229,16 +1229,15 @@ int sas_read_port_mode_page(struct scsi_device *sdev)
 	char *buffer = kzalloc(BUF_SIZE, GFP_KERNEL), *msdata;
 	struct sas_end_device *rdev = sas_sdev_to_rdev(sdev);
 	struct scsi_mode_data mode_data;
-	int res, error;
+	int error;
 
 	if (!buffer)
 		return -ENOMEM;
 
-	res = scsi_mode_sense(sdev, 1, 0x19, buffer, BUF_SIZE, 30*HZ, 3,
-			      &mode_data, NULL);
+	error = scsi_mode_sense(sdev, 1, 0x19, buffer, BUF_SIZE, 30*HZ, 3,
+				&mode_data, NULL);
 
-	error = -EINVAL;
-	if (!scsi_status_is_good(res))
+	if (error)
 		goto out;
 
 	msdata = buffer +  mode_data.header_length +
